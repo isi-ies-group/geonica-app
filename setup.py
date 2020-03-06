15a3446fe35c4649eae25841134645b4a314a2b8
m-nenkov <49555195+m-nenkov@users.noreply.github.com>
2020-03-06 12:38:54 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  6 11:30:00 2020
+
+@author: Martin
+"""
+import setuptools
+
+setuptools.setup(
+    name="lecturaBBDD", # Replace with your own username
+    version="0.1",
+    author="Martin",
+    author_email="m.nenkov@alumnos.upm.es",
+    description="Pequeña librería que facilita la lectura de la base de datos geonica",
+    # url="https://github.com/pypa/sampleproject",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Windows",
+    ],
+    python_requires='>=3.6',
+)
