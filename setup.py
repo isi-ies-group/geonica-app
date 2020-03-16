86441775b251a42e12fce6d5e1c940e83ba0855b
Ruben Nuñez <rubennj@users.noreply.github.com>
2020-03-16 16:31:39 +0100

    cambio nombre a pygeonica
    parece que el nombre con un guion no funciona bien

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ Created on Fri Mar  6 11:30:00 2020
 import setuptools
 
 setuptools.setup(
-    name="geonica-app", # Replace with your own username
+    name="pygeonica", # Replace with your own username
     version="0.1",
-    url='http://github.com/m-nenkov/geonica-app',
+    url='http://github.com/isi-ies-group/pygeonica',
     author="Martin",
     author_email="m.nenkov@alumnos.upm.es",
     description="Pequeña librería que facilita la lectura de la base de datos geonica",
@@ -18,7 +18,7 @@ setuptools.setup(
         "Operating System :: Windows",
     ],
     python_requires='>=3.6',
-    packages=['geonica-app'],
+    packages=['pygeonica'],
     zip_safe=False,
     package_data={'': ['*.txt','*.yaml']},
     include_package_data=True,
