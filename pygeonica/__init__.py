418fbba25c64d9d2e1d905fb7020c9aff0633d0d
Ruben Nuñez <rubennj@users.noreply.github.com>
2020-03-17 16:16:53 +0100

    Update __init__.py

--- pygeonica/__init__.py
+++ pygeonica/__init__.py
@@ -5,5 +5,5 @@ Created on Fri Mar  6 11:28:58 2020
 @author: Martin
 """
 
-# from pygeonica.estacion import *
-# from pygeonica.bbdd import *
+from pygeonica import estacion
+from pygeonica import bbdd
